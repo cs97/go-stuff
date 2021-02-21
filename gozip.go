986b8afd3c0f05902955daade486920240ca5693
leftside97 <l3f7s1d3@protonmail.com>
2021-02-21 16:38:59 +0100

    Update gozip.go

--- gozip.go
+++ gozip.go
@@ -1,28 +1,59 @@
-package main 
-  
-// Declare all the libraries which are needed. 
-import ( 
-    "compress/gzip"
+package main
+
+import (
     "fmt"
-    "os"
     "bufio"
+    "os"
     "io/ioutil"
-) 
-  
-func main() { 
-  fi, _ := os.Open(os.Args[1]) 
-  
-  read := bufio.NewReader(fi)
-  
-  data, _ := ioutil.ReadAll(read) 
-  
-  fo, _ := os.Create(os.Args[1]+".gz") 
-  
-  p := gzip.NewWriter(fo) 
-  
-  p.Write(data)
-  
-  p.Close() 
-  
-  fmt.Println("Done") 
+    "compress/gzip"
+    "bytes"
+)
+
+func writedata(data []byte, name string) {
+	fo, _ := os.Create(name) 
+	fo.Write(data)
+	fmt.Println("done")
 }
+
+func readdata(name string) []byte {
+	fi, _ := os.Open(name) 
+	read := bufio.NewReader(fi)
+	data, _ := ioutil.ReadAll(read) 
+	return data
+}
+
+func zip(data []byte) []byte {
+	var b bytes.Buffer
+    w := gzip.NewWriter(&b)
+    w.Write([]byte(data))
+    w.Close()
+    return b.Bytes()
+}
+
+func unzip(data []byte) []byte {
+	buf := bytes.NewBuffer(data)
+    reader, _ := gzip.NewReader(buf)
+	out, err := ioutil.ReadAll(reader)
+	if err != nil {
+        panic(err)
+    }
+    return out
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("usage: "+os.Args[0]+" [-zip|-unzip] <file>")
+		os.Exit(3)
+	}
+	switch os.Args[1] {
+	case "-zip":
+		writedata(zip(readdata(os.Args[2])),os.Args[2]+".zip")
+		fmt.Println("done")
+	case "-unzip":
+		writedata(unzip(readdata(os.Args[2])),os.Args[2]+".unzip")
+		fmt.Println("done")
+	default:
+		fmt.Println("usage: "+os.Args[0]+" [-zip|-unzip] <file>")
+	}	
+}
+
