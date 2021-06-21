44365e13942afac7056f0b8f23419297c4e8b9ed
leftside97 <l3f7s1d3@protonmail.com>
2021-06-21 22:23:20 +0200

    Create BinaryTree.go

---
+++ BinaryTree.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+ 	"fmt"
+)
+
+type node struct {
+    data int
+    l *node
+    r *node
+}
+
+func insert(n, x *node) {
+	if n.data == x.data {
+		return
+	}
+	
+	if n.data > x.data {
+		if n.l == nil {
+			n.l = x
+		} else {
+			insert(n.l, x)
+		}
+	} else {
+		if n.r == nil {
+			n.r = x
+		} else {
+			insert(n.r, x)
+		}
+	}
+}
+
+func main() {
+	tmp :=  &node{data: 69}
+	tmp2 := &node{data: 666}
+	tmp3 := &node{data: 13}
+	insert(tmp, tmp2)
+	insert(tmp, tmp3)	
+	
+	fmt.Println(tmp.data)
+	fmt.Println(tmp.r.data)
+	fmt.Println(tmp.l.data)
+	
+	
+	
+	
+}
